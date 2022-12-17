f68ba94148c82c6173ecadbb53caebd19094ce9b
Luis Velasquez <luis.velasquez.a@outlook.cl>
2022-12-17 14:19:19 -0300

    Subiendo archivos de tienda PymesMarketplace

---
+++ README.txt
@@ -0,0 +1,9 @@
+Hola muy buenas tardes Mi nombre es luis velásquez estudiante de la carrera de ingeniera en informatíca del instituto duoc UC, tengo 25 años y recien a mis 23 descubri el maravilloso mundo de la informatica,
+si estas leyendo esto me gustaria agradecer tu tiempo por revisar uno de mis proyectos
+en donde pongo en practica lo aprendido de manera autodidacta y en mi formacion academica
+
+En este proyecto Decidi utilizar HTML, CSS y Jquery para realizar validaciones a algunos campos del formulario
+de manera sencilla, Adicional a eso utilize en gran medida el display flex para ir organizando los elementos de
+nuestras cajas. para la galeria utilice flexbox, como funcionalidad extra me gustaria conectarme a una base de datos  para guardar los registros de manera persistente y de igual manera demostrar mi manejo en esta area
+
+Muchas gracias por tu tiempo
---
+++ js/form_jquery.js
@@ -0,0 +1,56 @@
+// $("#guardar").click(function(){
+//     alert('boton clickeado')
+// })
+
+
+
+$.validator.addMethod("terminaengmail", function(value, element, param){
+
+    if(value.endsWith(param)){
+        return true;
+    }
+        return false;
+    
+
+}, ("Debe terminar en {0}") )
+
+
+
+
+
+
+$("#form").validate({
+    rules: {
+        nombre:{
+            required: true,
+            minlength: 3,
+            maxlength: 30      
+        },
+        apellido:{
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        },
+        email:{
+            required: true,
+            email: true,
+            terminaengmail:"gmail.cl",
+        },
+        textarea: {
+            required: false
+        }
+    }
+})
+
+$("#guardar").click(function(){
+    if($("#form").valid() == false){
+        alert("No has llenado los todos los campos")
+        return;
+    }
+    let nombre = $("#nombre").val();
+    let apellido = $("#apellido").val();
+    let email = $("#email").val();
+    let comentario = $("#textarea").val();
+
+    console.log(nombre, apellido, email + "=>" + comentario )
+})
